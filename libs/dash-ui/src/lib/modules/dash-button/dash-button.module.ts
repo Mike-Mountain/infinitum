d570dc9488dc69daed812089633a536096cee753
Michael <mikemountain.dev@gmail.com>
2021-05-24 19:41:51 +0200

    INF-39 -> Update the dash-button to not use angular material

--- libs/dash-ui/src/lib/modules/dash-button/dash-button.module.ts
+++ libs/dash-ui/src/lib/modules/dash-button/dash-button.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from './components/button/button.component';
 import { ButtonClassPipe } from './pipes/button-class.pipe';
+import { ButtonDirective } from './directives/button.directive';
 
 
 @NgModule({
   declarations: [
     ButtonComponent,
-    ButtonClassPipe
+    ButtonClassPipe,
+    ButtonDirective
   ],
   imports: [
     CommonModule,
     MatButtonModule
   ],
   exports: [
-    ButtonComponent
+    ButtonComponent,
+    ButtonDirective
   ]
 })
 export class DashButtonModule {
